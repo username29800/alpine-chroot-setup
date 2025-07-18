3f0b3a9ddc0b69c99d8ff4f6e38cb511c9f490c4
username29800 <134399459+username29800@users.noreply.github.com>
2025-07-19 07:45:43 +0900

    install proot by default

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ echo 'nameserver 9.9.9.9' > /etc/resolv.conf
 echo 'nameserver 149.112.112.112' >> /etc/resolv.conf
 chmod 1777 /dev/shm
 apk update
-echo "$1"cmake "$1"make "$1"gcc "$1"g++ "$1"llvm20 "$1"clang20 "$1"gettext 7zip unzip curl zsh git python3 perl "$1"gawk "$1"m4 "$1"autoconf xz sudo vim xterm gcompat dbus dbus-x11 ibus ibus-hangul firefox font-noto-cjk font-noto-emoji sof-firmware alsa-ucm-conf xrandr tigervnc pulseaudio pavucontrol-qt "$1"bash | tr ' ' \\n > list
+echo "$1"cmake "$1"make "$1"gcc "$1"g++ "$1"llvm20 "$1"clang20 "$1"gettext 7zip unzip curl zsh git python3 perl "$1"gawk "$1"m4 "$1"autoconf xz sudo vim xterm gcompat dbus dbus-x11 ibus ibus-hangul firefox font-noto-cjk font-noto-emoji sof-firmware alsa-ucm-conf xrandr tigervnc pulseaudio pavucontrol-qt "$1"bash "$1"proot | tr ' ' \\n > list
 apk search mate | grep \^mate | cut -d. -f-1 | sed 's,-\d*\?$,,' >> list
 apk search eudev | grep \^eudev | cut -d. -f-1 | sed 's,-\d*\?$,,' >> list
 apk search libcanberra | grep \^lib | cut -d. -f-1 | sed 's,-\d*\?$,,' >> list
