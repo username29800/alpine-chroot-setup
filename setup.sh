9c8bbc4fe3d15c1aa7b4a71fe90e7192a33814fc
username29800 <134399459+username29800@users.noreply.github.com>
2025-07-22 20:43:32 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,8 +17,8 @@ sed -i "1i#\!/bin/sh" list
 sed -i 's,^.*-lang$,,' list
 sed -i 's,^.*-doc$,,' list
 sed -i 's,^.*-dev$,,' list
-sed -i 's,^.*$,apk add &,' list
 sed -i 'N;s,\n, ,' list
+sed -i 's,^.*$,apk add &,' list
 sed -i 's,^apk add #.*\?$,,' list
 chmod 500 list
 ./list
