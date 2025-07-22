2b3bc00b5f14e06fb40be287a9e58351a9499416
username29800 <134399459+username29800@users.noreply.github.com>
2025-07-22 21:49:19 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ echo 'nameserver 9.9.9.9' > /etc/resolv.conf
 echo 'nameserver 149.112.112.112' >> /etc/resolv.conf
 chmod 1777 /dev/shm
 apk update
-echo "$1"cmake "$1"make "$1"gcc "$1"g++ "$1"llvm20 "$1"clang20 "$1"gettext 7zip unzip curl zsh git python3 perl "$1"gawk "$1"m4 "$1"autoconf xz sudo vim xterm gcompat dbus dbus-x11 ibus ibus-hangul firefox font-noto-cjk font-noto-emoji sof-firmware alsa-ucm-conf xrandr tigervnc pulseaudio pavucontrol-qt "$1"bash "$1"proot | tr ' ' \\n > list
+echo flatpak "$1"cmake "$1"make "$1"gcc "$1"g++ "$1"llvm20 "$1"clang20 "$1"gettext 7zip unzip curl zsh git python3 perl "$1"gawk "$1"m4 "$1"autoconf xz sudo vim xterm gcompat dbus dbus-x11 ibus ibus-hangul firefox font-noto-cjk font-noto-emoji sof-firmware alsa-ucm-conf xrandr tigervnc pulseaudio pavucontrol-qt "$1"bash "$1"proot | tr ' ' \\n > list
 apk search mate | grep \^mate | grep -v ^materia | sed 's,[-_]\d.*,,' >> list
 apk search eudev | grep \^eudev | sed 's,[-_]\d.*,,' >> list
 apk search libcanberra | grep \^lib | sed 's,[-_]\d.*,,' >> list
@@ -97,6 +97,7 @@ echo "#\!/bin/sh" > /home/user/theme
 echo 'safefox https://www.pling.com/p/1267246/ &' >> /home/user/theme
 echo 'safefox https://www.pling.com/p/1598493/ &' >> /home/user/theme
 chmod 700 /home/user/theme
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 cd /build
 git clone https://github.com/naver/d2codingfont
 git clone https://github.com/ohmyzsh/ohmyzsh
