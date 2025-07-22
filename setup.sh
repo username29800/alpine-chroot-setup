157e38bf7bc6a4544ef3900cb6237200c2b1fa3d
username29800 <134399459+username29800@users.noreply.github.com>
2025-07-22 20:38:47 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,11 +6,11 @@ echo 'nameserver 149.112.112.112' >> /etc/resolv.conf
 chmod 1777 /dev/shm
 apk update
 echo "$1"cmake "$1"make "$1"gcc "$1"g++ "$1"llvm20 "$1"clang20 "$1"gettext 7zip unzip curl zsh git python3 perl "$1"gawk "$1"m4 "$1"autoconf xz sudo vim xterm gcompat dbus dbus-x11 ibus ibus-hangul firefox font-noto-cjk font-noto-emoji sof-firmware alsa-ucm-conf xrandr tigervnc pulseaudio pavucontrol-qt "$1"bash "$1"proot | tr ' ' \\n > list
-apk search mate | grep \^mate | grep -v ^materia | cut -d. -f-1 | sed 's,-\d*\?$,,' >> list
-apk search eudev | grep \^eudev | cut -d. -f-1 | sed 's,-\d*\?$,,' >> list
-apk search libcanberra | grep \^lib | cut -d. -f-1 | sed 's,-\d*\?$,,' >> list
-apk search xf86-video | grep \^xf86 | cut -d. -f-1 | sed 's,-\d*\?$,,' | sed 's,^,'"$1"',' >> list
-apk search zsh | grep \^zsh | cut -d. -f-1 | sed 's,-\d*\?$,,' | sed 's,^,'"$1"',' >> list
+apk search mate | grep \^mate | grep -v ^materia | sed 's,[-_]\d.*,,' >> list
+apk search eudev | grep \^eudev | sed 's,[-_]\d.*,,' >> list
+apk search libcanberra | grep \^lib | sed 's,[-_]\d.*,,' >> list
+apk search xf86-video | grep \^xf86 | sed 's,[-_]\d.*,,' | sed 's,^,'"$1"',' >> list
+apk search zsh | grep \^zsh | sed 's,[-_]\d.*,,' | sed 's,^,'"$1"',' >> list
 sed -i '2s,^.*$,&\n&,' /etc/apk/repositories
 sed -i '3s,alpine/.*$,alpine/edge/testing,' /etc/apk/repositories
 sed -i 's,^,apk add ,' list
@@ -18,6 +18,7 @@ sed -i "1i#\!/bin/sh" list
 sed -i 's,^.*-lang$,,' list
 sed -i 's,^.*-doc$,,' list
 sed -i 's,^.*-dev$,,' list
+sed -i 'N;s,\n, ,' list
 sed -i 's,^apk add #.*\?$,,' list
 chmod 500 list
 ./list
