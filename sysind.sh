5e3180f3ac0cbf4daf769d7a11dd3451e93f5e9b
username29800 <134399459+username29800@users.noreply.github.com>
2025-07-23 14:19:40 +0900

    Update sysind.sh

--- sysind.sh
+++ sysind.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#prerequisites: unzip, coreutils or busybox firefox, tigervnc, gsettings, dconf, mate-desktop + mate-terminal, sudo, vim, git, zsh, user 'user' with home directory
+#prerequisites: unzip, coreutils or busybox firefox, openbox, xterm, tigervnc, sudo, vim, git, zsh, user 'user' with home directory
 mkdir /build
 cd /build
 chmod 1777 /dev/shm
@@ -17,34 +17,16 @@ echo 'export XMODIFIERS=@im=ibus' >> .zshrc.1
 echo 'export PULSE_SERVER=localhost' >> .zshrc.1
 cp .zshrc.1 .config/tigervnc/xstartup
 echo 'xrandr --output VNC-0 --mode 1920x1080' >> .config/tigervnc/xstartup
-echo 'mate-session' >> .config/tigervnc/xstartup
+echo 'openbox' >> .config/tigervnc/xstartup
 ln -sf .config/tigervnc/xstartup
 echo '#!/bin/sh' > /bin/safefox
 echo 'export MOZ_FAKE_NO_SANDBOX=1' >> /bin/safefox
 echo 'firefox $1' >> /bin/safefox
 chmod 755 /bin/safefox
 chmod 744 .config/tigervnc/xstartup
+mkdir .vnc
+cp xstartup .vnc/xstartup
 mkdir /usr/share/xsessions
-echo "#\!/bin/sh" > /home/user/mate-config
-echo "vncserver :100 /dev/null 2>&1 &" >> /home/user/mate-config
-echo "export DISPLAY=:100" >> /home/user/mate-config
-echo "gsettings set org.mate.interface font-name 'Noto Sans CJK KR 11'" >> /home/user/mate-config
-echo "gsettings set org.mate.interface document-font-name 'Noto Sans CJK KR 11'" >> /home/user/mate-config
-echo "gsettings set org.mate.interface monospace-font-name 'D2Coding 12'" >> /home/user/mate-config
-echo dconf write "/org/mate/terminal/profiles/default/font \"'D2Coding 12'\"" >> /home/user/mate-config
-echo dconf write "/org/mate/terminal/profiles/default/use-system-font \"false\"" >> /home/user/mate-config
-echo dconf write "/org/mate/terminal/profiles/default/use-theme-colors \"false\"" >> /home/user/mate-config
-echo dconf write "/org/mate/terminal/profiles/default/foreground-color \"'#FFFFFFFFFFFF'\"" >> /home/user/mate-config
-echo dconf write "/org/mate/terminal/profiles/default/background-color \"'#000000000000'\"" >> /home/user/mate-config
-echo dconf write "/org/mate/terminal/profiles/default/background-type \"'transparent'\"" >> /home/user/mate-config
-echo dconf write "/org/mate/terminal/profiles/default/background-darkness \"0.66\"" >> /home/user/mate-config
-echo dconf write "/org/mate/terminal/profiles/default/palette \"'#000000000000:#CCCC00000000:#4E4D9A9A0605:#C4C3A0A00000:#34346564A4A3:#7575504F7B7B:#060598979A9A:#D3D3D7D6CFCF:#555457565352:#EFEF29282928:#8A89E2E23434:#FCFBE9E84F4F:#72729F9ECFCF:#ADAC7F7EA8A8:#3434E2E2E2E2:#EEEDEEEDECEB'\"" >> /home/user/mate-config
-echo "gsettings set org.mate.Marco.general theme 'Menta'" >> /home/user/mate-config
-echo "gsettings set org.mate.Marco.general titlebar-uses-system-font 'false'" >> /home/user/mate-config
-echo "gsettings set org.mate.Marco.general titlebar-font 'Noto Sans CJK KR 11'" >> /home/user/mate-config
-echo "kill %1" >> /home/user/mate-config
-chmod 755 /home/user/mate-config
-su -lc 'dbus-launch sh /home/user/mate-config' user
 echo '[Desktop Entry]' > /usr/share/xsessions/custom.desktop
 echo 'Name=Custom' >> /usr/share/xsessions/custom.desktop
 echo 'Exec=/home/user/xstartup' >> /usr/share/xsessions/custom.desktop
@@ -71,12 +53,6 @@ echo "chmod 666 /dev/pts/ptmx" >> /initpty
 echo "su -l "'"$1"' >> /initpty
 echo '"$2"'"umount /dev/pts" >> /initpty
 chmod 744 /initpty
-echo 'SendPrimary=0' >> /etc/tigervnc/vncserver-config-mandatory
-echo "#\!/bin/sh" > /home/user/theme
-echo 'safefox https://www.pling.com/p/1267246/ &' >> /home/user/theme
-echo 'safefox https://www.pling.com/p/1598493/ &' >> /home/user/theme
-chmod 700 /home/user/theme
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 cd /build
 git clone https://github.com/naver/d2codingfont
 git clone https://github.com/ohmyzsh/ohmyzsh
