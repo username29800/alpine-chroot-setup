56b29f2f518046f8c18cb61c67918de962c08bbf
username29800 <clockway84@proton.me>
2025-09-23 09:29:14 +0900

    linux-sidearm extension #2

--- pkg-alpine-base.sh
+++ pkg-alpine-base.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 mkdir /build
 cd /build
-echo 'nameserver 9.9.9.9' > /etc/resolv.conf
-echo 'nameserver 149.112.112.112' >> /etc/resolv.conf
+echo 'nameserver 1.1.1.2' > /etc/resolv.conf
+echo 'nameserver 1.0.0.2' >> /etc/resolv.conf
 chmod 1777 /dev/shm
 apk update
-echo openssh openbox xfe qterminal rofi mousepad gnome-clocks "$1"cmake "$1"make "$1"gcc "$1"g++ "$1"llvm20 "$1"clang20 "$1"gettext 7zip unzip curl zsh git python3 perl "$1"gawk "$1"m4 "$1"autoconf xz sudo vim xterm gcompat dbus dbus-x11 ibus ibus-hangul firefox font-noto-cjk font-noto-emoji sof-firmware alsa-ucm-conf xrandr tigervnc pulseaudio pavucontrol-qt "$1"bash "$1"proot | tr ' ' \\n > list
+echo openssh openbox xfe qterminal rofi mousepad 7zip unzip curl zsh git python3 perl gawk xz sudo vim xterm gcompat dbus dbus-x11 ibus ibus-hangul firefox font-noto-cjk font-noto-emoji sof-firmware alsa-ucm-conf xrandr tigervnc pulseaudio pavucontrol-qt | tr ' ' \\n > list
 apk search eudev | grep \^eudev | sed 's,[-_]\d.*,,' >> list
-#apk search libcanberra | grep \^lib | sed 's,[-_]\d.*,,' >> list
 apk search xf86-video | grep \^xf86 | sed 's,[-_]\d.*,,' | sed 's,^,'"$1"',' >> list
 apk search zsh | grep \^zsh | sed 's,[-_]\d.*,,' | sed 's,^,'"$1"',' >> list
 sed -i '2s,^.*$,&\n&,' /etc/apk/repositories
@@ -20,6 +19,7 @@ sed -i 's,^.*$,apk add &,' list
 sed -i 's,^apk add #.*\?$,,' list
 chmod 500 list
 ./list
-echo 'add user: username must be user'
+echo 'add user manually: username must be user'
+echo '(command may differ by distribution)'
 #echo "adduse.r/psw.d/
 #adduse.r/psw.d/" | adduser user
--- setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sh sys-alpine.sh
-sh sysind.sh
-exit
