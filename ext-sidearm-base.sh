882c420b5195a9bebc4d24c031a63d801e295a57
username29800 <piapia78490@gmail.com>
2025-09-28 11:59:26 +0900

    linux-sidearm extension #3-0

---
+++ ext-sidearm-base.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#prerequisites:
+#user 'sidearmconnect'(without password)
+#add password customization for additional security
+#usage: [script path] [password extension] [hostkey(private)]
+echo 'GoPythonFluttter/%025%'"$1"'
+GoPythonFlutter/%025%'"$1" | passwd sidearmconnect
+mkdir /sacsshhkey /sacsshpkey
+ln -sf /home/sidearmconnect/"$2" /sacsshhkey/key
+ln -sf /home/sidearmconnect/"$2".pub /sacsshpkey/key
+ln -sf /utils/sasshsvl /home/sidearmconnect/listen
+ln -sf /utils/sasshsvl /bin/saclisten
+chown -R sidearmconnect /home/sidearmconnect
+chown sidearmconnect /bin/saclisten
+chmod 744 /bin/saclisten
