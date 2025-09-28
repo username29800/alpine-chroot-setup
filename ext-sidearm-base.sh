89b91345657f5a9f18e2f3e947e2efa3c23fa96f
username29800 <134399459+username29800@users.noreply.github.com>
2025-09-28 13:18:09 +0900

    Update ext-sidearm-base.sh

--- ext-sidearm-base.sh
+++ ext-sidearm-base.sh
@@ -5,11 +5,13 @@
 #usage: [script path] [password extension] [hostkey(private)]
 mkdir /home/sidearmconnect
 chmod -R 755 /home/sidearmconnect
+rm /sacsshhkey/key
+rm /sacsshpkey/key
 echo 'GoPythonFlutter/%025%'"$1"'
 GoPythonFlutter/%025%'"$1" | passwd sidearmconnect
 mkdir /sacsshhkey /sacsshpkey
-ln -sf "$(pwd | sed s,/$,,)/$2" /sacsshhkey/key
-ln -sf "$(pwd | sed s,/$,,)/$2".pub /sacsshpkey/key
+ln -sf "$(pwd | sed s,/$,,)"/"$2" /sacsshhkey/key
+ln -sf "$(pwd | sed s,/$,,)"/"$2".pub /sacsshpkey/key
 ln -sf /utils/sasshsvl /home/sidearmconnect/listen
 ln -sf /utils/sasshsvl /bin/saclisten
 chown -R sidearmconnect /home/sidearmconnect
